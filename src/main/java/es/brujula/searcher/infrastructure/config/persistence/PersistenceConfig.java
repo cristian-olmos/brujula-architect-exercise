d395c02c687eb963fbeaaf06fd77afa80540380b
Cristian Olmos <cristian.olmos@melia.com>
2020-08-23 19:41:22 +0200

    NEW: adding persistence config

--- src/main/java/es/brujula/searcher/infrastructure/config/persistence/PersistenceConfig.java
+++ src/main/java/es/brujula/searcher/infrastructure/config/persistence/PersistenceConfig.java
@@ -1,59 +1,32 @@
 package es.brujula.searcher.infrastructure.config.persistence;
 
-import org.apache.ibatis.mapping.VendorDatabaseIdProvider;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.type.JdbcType;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.jndi.JndiObjectFactoryBean;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
-import javax.naming.NamingException;
 import javax.sql.DataSource;
-import java.util.Properties;
 
 @Configuration
-@Import(PersistenceProperties.class)
 @MapperScan("es.brujula.searcher.infrastructure.persistence.mybatis.mapper")
 public class PersistenceConfig {
 
-    private static final String POSTGRESQL_KEY = "PostgreSQL";
-    private static final String POSTGRESQL_VALUE = "postgresql";
-    private static final String ORACLE_KEY = "Oracle";
-    private static final String ORACLE_VALUE = "oracle";
-
-//    @Bean(name = "dataSource")
-//    public DataSource dataSource(PersistenceProperties persistenceProperties) throws NamingException {
-//        JndiObjectFactoryBean bean = new JndiObjectFactoryBean();
-//        bean.setJndiName(persistenceProperties.getDataSource().getJndiName());
-//        bean.setProxyInterface(DataSource.class);
-//        bean.setLookupOnStartup(false);
-//        bean.afterPropertiesSet();
-//        return (DataSource) bean.getObject();
-//    }
-
     @Bean
-    public VendorDatabaseIdProvider vendorDatabaseIdProvider() {
-        Properties vendorProperties = new Properties();
-        vendorProperties.setProperty(POSTGRESQL_KEY, POSTGRESQL_VALUE);
-        vendorProperties.setProperty(ORACLE_KEY, ORACLE_VALUE);
-        VendorDatabaseIdProvider dbiIdProvider = new VendorDatabaseIdProvider();
-        dbiIdProvider.setProperties(vendorProperties);
-        return dbiIdProvider;
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("schema.sql")
+                .addScript("data.sql")
+                .build();
     }
-
+ 
     @Bean
-    public SqlSessionFactory sqlSessionFactory(DataSource dataSource,
-                                               ApplicationContext applicationContext,
-                                               VendorDatabaseIdProvider vendorDatabaseIdProvider) throws Exception {
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
         SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
-        factoryBean.setDataSource(dataSource);
-        factoryBean.setDatabaseIdProvider(vendorDatabaseIdProvider);
-        SqlSessionFactory sqlSessionFactory = factoryBean.getObject();
-        sqlSessionFactory.getConfiguration().setJdbcTypeForNull(JdbcType.NULL);
-        return sqlSessionFactory;
+        factoryBean.setDataSource(dataSource());
+        return factoryBean.getObject();
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,58 +1,24 @@
-DROP TABLE IF EXISTS HOTELS;
-
-CREATE TABLE HOTELS (
-  id VARCHAR(250) PRIMARY KEY,
-  name VARCHAR(250) NOT NULL,
-  category VARCHAR(250) NOT NULL,
-  address VARCHAR(250) NOT NULL
-);
-
 INSERT INTO HOTELS (id, name, category, address) VALUES
   ('09cdda32-e239-11ea-87d0-0242ac130003', 'Melia Granada', '4', 'Calle Ángel Ganivet, 7, 18009 Granada'),
   ('1392fc82-e239-11ea-87d0-0242ac130003', 'Hospes Palacio de los Patos', '5', 'Calle Solarillo de Gracia, 1, 18002 Granada'),
   ('1c6f010c-e239-11ea-87d0-0242ac130003', 'Alhambra Palace', '5', 'Plaza Arquitecto García de Paredes, 1, 18009 Granada');
-
-DROP TABLE IF EXISTS ROOMS;
-
-CREATE TABLE ROOMS (
-  id VARCHAR(250) PRIMARY KEY,
-  hotel_id VARCHAR(250) FOREIGN KEY (hotel_id) REFERENCES HOTELS(id),
-  name VARCHAR(250) NOT NULL,
-  occupation number NOT NULL,
-  price DECIMAL(7,2) NOT NULL
-);
-
 INSERT INTO ROOMS (id, hotel_id, name, occupation, price) VALUES
   ('bcb9a530-e32a-11ea-87d0-0242ac130003', '09cdda32-e239-11ea-87d0-0242ac130003', 'Habitación Melià triple', 3, 120.25),
   ('c406734a-e32a-11ea-87d0-0242ac130003', '09cdda32-e239-11ea-87d0-0242ac130003', 'Habitación clásica con terraza', 2, 100.50),
-  ('c9fcba0c-e32a-11ea-87d0-0242ac130003', '09cdda32-e239-11ea-87d0-0242ac130003', 'Habitación clásica' 2, 90.75);
-  ('d140ee14-e32a-11ea-87d0-0242ac130003', '1392fc82-e239-11ea-87d0-0242ac130003', 'Dreamer''s' 2, 100.95);
-  ('d140ee14-e32a-11ea-87d0-0242ac130003', '1392fc82-e239-11ea-87d0-0242ac130003', 'Dreamer''s Palacio' 2, 180.35);
-  ('d9af1fda-e32a-11ea-87d0-0242ac130003', '1c6f010c-e239-11ea-87d0-0242ac130003', 'Junior Suite' 2, 200.50);
-  ('df683024-e32a-11ea-87d0-0242ac130003', '1c6f010c-e239-11ea-87d0-0242ac130003', 'Suite' 3, 290.85);
-
-DROP TABLE IF EXISTS SERVICES;
-
-CREATE TABLE SERVICES (
-  id VARCHAR(250) PRIMARY KEY,
-  name VARCHAR(250) NOT NULL,
-);
-
+  ('c9fcba0c-e32a-11ea-87d0-0242ac130003', '09cdda32-e239-11ea-87d0-0242ac130003', 'Habitación clásica', 2, 90.75),
+  ('d140ee14-e32a-11ea-87d0-0242ac130003', '1392fc82-e239-11ea-87d0-0242ac130003', 'Dreamer''s', 2, 100.95),
+  ('d141ee14-e32a-11ea-87d0-0242ac130003', '1392fc82-e239-11ea-87d0-0242ac130003', 'Dreamer''s Palacio', 2, 180.35),
+  ('d9af1fda-e32a-11ea-87d0-0242ac130003', '1c6f010c-e239-11ea-87d0-0242ac130003', 'Junior Suite', 2, 200.50),
+  ('df683024-e32a-11ea-87d0-0242ac130003', '1c6f010c-e239-11ea-87d0-0242ac130003', 'Suite', 3, 290.85);
 INSERT INTO SERVICES (id, name) VALUES
   ('bf247dca-e32c-11ea-87d0-0242ac130003', 'Swimming Pool'),
   ('c6b80f48-e32c-11ea-87d0-0242ac130003', 'Spa'),
   ('cb91fcc2-e32c-11ea-87d0-0242ac130003', 'Gym');
-
-DROP TABLE IF EXISTS HOTELS_SERVICES;
-
-CREATE TABLE HOTELS_SERVICES (
-  hotel_id VARCHAR(250)  FOREIGN KEY (hotel_id) REFERENCES HOTELS(id),
-  service_id VARCHAR(250)  FOREIGN KEY (service_id) REFERENCES SERVICES(id)
-);
-
 INSERT INTO HOTELS_SERVICES (hotel_id, service_id) VALUES
   ('09cdda32-e239-11ea-87d0-0242ac130003', 'c6b80f48-e32c-11ea-87d0-0242ac130003'),
   ('09cdda32-e239-11ea-87d0-0242ac130003', 'cb91fcc2-e32c-11ea-87d0-0242ac130003'),
-  ('1392fc82-e239-11ea-87d0-0242ac130003', 'bf247dca-e32c-11ea-87d0-0242ac130003');
-  ('1392fc82-e239-11ea-87d0-0242ac130003', 'cb91fcc2-e32c-11ea-87d0-0242ac130003');
+  ('1392fc82-e239-11ea-87d0-0242ac130003', 'bf247dca-e32c-11ea-87d0-0242ac130003'),
+  ('1392fc82-e239-11ea-87d0-0242ac130003', 'cb91fcc2-e32c-11ea-87d0-0242ac130003'),
   ('1c6f010c-e239-11ea-87d0-0242ac130003', 'bf247dca-e32c-11ea-87d0-0242ac130003');
+
+  COMMIT;
